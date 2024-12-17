e2b152905654e649214d0c8f6d45efcb0f1e91b4
Latya-fay <ndanumulatya7@gmail.com>
2024-12-17 10:42:20 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+// Function to fetch the books from the Game of Thrones API
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch request to the API
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+  const main = document.querySelector("main");
+
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks();
